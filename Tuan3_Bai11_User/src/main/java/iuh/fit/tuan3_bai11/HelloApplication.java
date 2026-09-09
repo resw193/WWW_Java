fb03c907187d0284677c0430e8e04ae97a58eee4
Bảo Đinh <baodinh.nguyen321@gmail.com>
2026-09-09 19:21:57 +0700

    add bai 11 test api user bang postman

---
+++ Tuan3_Bai11_User/src/main/java/iuh/fit/tuan3_bai11/HelloApplication.java
@@ -0,0 +1,9 @@
+package iuh.fit.tuan3_bai11;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
